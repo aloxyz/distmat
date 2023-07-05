2a4615518b0b29b2cb3f2a3227bcde734dd2a899
veetiz <34392973+veetiz@users.noreply.github.com>
2023-07-05 10:54:27 +0200

    fix

--- startup/cmdtype.py
+++ startup/cmdtype.py
@@ -4,5 +4,5 @@ from argparse import ArgumentTypeError
 def unsigned(x):
     x = int(x)
     if x < 0:
-        raise ArgumentTypeError("Minimum bandwidth is 12")
+        raise ArgumentTypeError("Il valore inserito deve essere > 0")
     return x
