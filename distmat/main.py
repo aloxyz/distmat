ef6a500d60d6654fab3bc97f763904ea1957b480
Gabriele Aloisio <aloisiogabriele@gmail.com>
2023-09-29 13:52:32 +0200

    tests draft

--- distmat/main.py
+++ distmat/main.py
@@ -2,10 +2,58 @@ import ray
 import logging
 from matrix import Matrix
 import numpy as np
+import time
+import csv
+
+def test_dot_range(l, u, runs):
+    measures = []
+    sub_runs = []
+    for n in range(l, u):
+        for i in range(runs):
+            a = Matrix.random_int(16, 16, -10**n, 10**n)
+            b = Matrix.random_float(16, 16, -10**n, 10**n)
+
+            t0 = time.time()
+            Matrix.dot(a,b)
+            t1 = time.time()
+
+            sub_runs.append(t1-t0)
+            print(t1-t0)
+
+        measures.append(t1-t0)
+
+    return measures
+    
+def test_dot_size(l, u, runs):
+    measures = []
+    sub_runs = []
+    for n in range(l, u):
+        for i in range(runs):
+            a = Matrix.random_int(n, n, -99, 99)
+            b = Matrix.random_float(n, n, -99, 99)
+
+            t0 = time.time()
+            Matrix.dot(a,b)
+            t1 = time.time()
+
+            sub_runs.append(t1-t0)
+            print(t1-t0)
+
+        measures.append(sub_runs)
+
+    return measures
+
 
 if __name__ == "__main__":
-    a = Matrix.random_int(8, 8, -99, 99)
-    b = Matrix.random_float(8, 8, -99, 99)
+    if ray.is_initialized:
+        ray.shutdown()
+    ray.init(include_dashboard=True)
+
+    test_results = test_dot_size(2, 8, 4)    
+
+    with open("output.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(test_results)
 
     # DOT PRODUCT
     # print("\ndistmat:\n", Matrix.dot(a,b))
@@ -20,10 +68,6 @@ if __name__ == "__main__":
     # print("distmat: ", a.rank())
 
     # INVERSE
-    print("\ndistmat:\n", a.inv())
-    print("numpy: \n", np.linalg.inv(np.array(a.get())))
-
-    if ray.is_initialized:
-        ray.shutdown()
-    ray.init(include_dashboard=True)
+    # print("\ndistmat:\n", a.inv())
+    # print("numpy: \n", np.linalg.inv(np.array(a.get())))
     
