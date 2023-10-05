425b5eff34d95db0166c3877fa0c37d79c07096c
Gabriele Aloisio <aloisiogabriele@gmail.com>
2023-10-05 12:27:07 +0200

    updated paper

--- distmat/main.py
+++ distmat/main.py
@@ -3,9 +3,9 @@ from matrix import Matrix
 import time
 import csv
     
-def test_dot_size(l, u, runs):
+def test_dot(l, u, runs):
     '''Make tests for matrices size l...u'''
-    max_n = (u-l)*5
+    max_n = (u-l)*runs
     counter = 0
     measures = []
     
@@ -14,7 +14,7 @@ def test_dot_size(l, u, runs):
         sub_run.append(n) # matrix size
 
         for i in range(runs):
-            a = Matrix.random_int(n, n, -10**16, 10**16)
+            a = Matrix.random_int(n, n, -10**8, 10**8)
             b = Matrix.random_float(n, n, -99, 99)
 
             t0 = time.time()
@@ -30,9 +30,9 @@ def test_dot_size(l, u, runs):
 
     return measures
 
-def test_det_size(l, u, runs):
+def test_det(l, u, runs):
     '''Make tests for matrices size l...u'''
-    max_n = (u-l)*5
+    max_n = (u-l)*runs
     counter = 0
     measures = []
     
@@ -41,11 +41,10 @@ def test_det_size(l, u, runs):
         sub_run.append(n) # matrix size
 
         for i in range(runs):
-            a = Matrix.random_int(n, n, -10**16, 10**16)
-            b = Matrix.random_float(n, n, -99, 99)
+            a = Matrix.random_float(n, n, -10**8, 10**8)
 
             t0 = time.time()
-            Matrix.dot(a,b)
+            a.det()
             t1 = time.time()
 
             sub_run.append(t1-t0)
@@ -57,33 +56,70 @@ def test_det_size(l, u, runs):
 
     return measures
 
+def test_rank(l, u, runs):
+    '''Make tests for matrices size l...u'''
+    max_n = (u-l)*runs
+    counter = 0
+    measures = []
+    
+    for n in range(l, u):
+        sub_run = []
+        sub_run.append(n) # matrix size
 
+        for i in range(runs):
+            a = Matrix.random_float(n, n, -10**8, 10**8)
+
+            t0 = time.time()
+            a.rank()
+            t1 = time.time()
+
+            sub_run.append(t1-t0)
+
+            counter += 1
+            print(f"run # {counter}/{max_n}: {t1-t0}")
+
+        measures.append(sub_run)
+
+    return measures
+
+def test_inv(l, u, runs):
+    '''Make tests for matrices size l...u'''
+    max_n = (u-l)*runs
+    counter = 0
+    measures = []
+    
+    for n in range(l, u):
+        sub_run = []
+        sub_run.append(n) # matrix size
+
+        for i in range(runs):
+            a = Matrix.random_float(n, n, -10**8, 10**8)
+
+            t0 = time.time()
+            a.inv()
+            t1 = time.time()
+
+            sub_run.append(t1-t0)
+
+            counter += 1
+            print(f"run # {counter}/{max_n}: {t1-t0}")
+
+        measures.append(sub_run)
+
+    return measures
 
 if __name__ == "__main__":
     if ray.is_initialized:
         ray.shutdown()
     ray.init(include_dashboard=True)
 
-    # test_results = test_dot_size(2, 30, 5)
-    test_results = test_dot_size(2, 30, 5)
+    # test_results = test_inv(2, 9, 3)
 
-    with open("test_results/test_dot_size_2_30_5.csv", "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(test_results)
+    # with open("test_results/test_inv_2_20_3.csv", "w") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(test_results)
 
-    # DOT PRODUCT
-    # print("\ndistmat:\n", Matrix.dot(a,b))
-    # print("numpy: \n", np.dot(np.array(a.get()), np.array(b.get())))
-
-    # DETERMINANT
-    # print("\nnumpy: ", np.linalg.det(np.array(a.get())))
-    # print("distmat: ", a.det())
-
-    # RANK
-    # print("\nnumpy: ", np.linalg.matrix_rank(np.array(a.get())))
-    # print("distmat: ", a.rank())
-
-    # INVERSE
-    # print("\ndistmat:\n", a.inv())
-    # print("numpy: \n", np.linalg.inv(np.array(a.get())))
-    
+    a = Matrix.random_float(8, 8, -10**8, 10**8)
+    t0 = time.time()
+    a.inv()
+    print(time.time() - t0)
